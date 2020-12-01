b810aaf876c53a7054e4a1a8257099e72599f354
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-01 01:10:35 -0500

    IStructure.cs

---
+++ game/Assets/Scripts/Interfaces/IStructure.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace HeroClash {
+  internal interface IStructure {
+    float Integrity { get; set; }
+    float SpawnRate { get; }
+    TEAM Team { get; set; }
+
+    void OnTriggerEnter(Collider collider);
+    void OnTriggerExit(Collider collider);
+    IEnumerator Spawn();
+  }
+}
