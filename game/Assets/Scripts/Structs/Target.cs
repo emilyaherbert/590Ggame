4a4a78b2bc29ff4b910ac11f2112942c801de2eb
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-01 01:12:39 -0500

    ICharacter.cs & Target.cs

---
+++ game/Assets/Scripts/Structs/Target.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+namespace HeroClash {
+  internal struct Target {
+    internal Collider Box { get; }
+    internal ICharacter Character { get; }
+    internal IStructure Structure { get; }
+
+    internal Target(Collider c, ICharacter ch) {
+      Box = c;
+      Character = ch;
+      Structure = null;
+    }
+
+    internal Target(Collider c, IStructure st) {
+      Box = c;
+      Character = null;
+      Structure = st;
+    }
+  }
+}
