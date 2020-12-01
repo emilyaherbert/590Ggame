0943b88bd0a96014ee77236e64feaf5daa4d29c1
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-01 01:12:39 -0500

    ICharacter.cs & Target.cs

---
+++ game/Assets/Scripts/Interfaces/ICharacter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace HeroClash {
+  internal interface ICharacter {
+    Stat Self { get; set; }
+    Target Them { get; set; }
+    STATE State { get; set; }
+    TEAM Team { get; set; }
+
+    void Move(Vector3 vector3);
+    IEnumerator Attack();
+  }
+}
