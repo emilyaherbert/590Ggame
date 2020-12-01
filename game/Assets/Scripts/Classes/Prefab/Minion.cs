c00be846cb129ca8646c4333038241ac343bd6c2
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-01 01:18:20 -0500

    Minion.cs

---
+++ game/Assets/Scripts/Classes/Prefab/Minion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace HeroClash {
+  internal class Minion : MonoBehaviour, ICharacter {
+    public Stat Self { get; set; }
+    public Target Them { get; set; }
+    public STATE State { get; set; }
+    public TEAM Team { get; set; }
+
+    public void Move(Vector3 vector3) {
+      throw new System.NotImplementedException();
+    }
+
+    public IEnumerator Attack() {
+      throw new System.NotImplementedException();
+    }
+  }
+}
