e3452d342503d5edd623574ff0c91d6a8c240889
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-01 01:17:00 -0500

    Player.cs

---
+++ game/Assets/Scripts/Classes/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+namespace HeroClash {
+  internal class Player : MonoBehaviour {
+    internal Hero hero;
+
+    private void Start() {
+      // TODO: delete stmt; set in GameManager.cs
+      hero = gameObject.GetComponent<TempHero>();
+    }
+
+    private void Update() {
+      if (Input.GetMouseButtonDown(0) &&
+          Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
+            out RaycastHit hit) &&
+          !hit.collider.CompareTag("Player") &&
+          !hit.collider.CompareTag("Character") &&
+          !hit.collider.CompareTag("Structure")) {
+        hero.Move(hit.point);
+      } else if (Input.GetMouseButtonDown(1) &&
+          Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
+            out hit) &&
+          !hit.collider.CompareTag("Player") &&
+          (hit.collider.CompareTag("Character") ||
+          hit.collider.CompareTag("Structure"))) {
+        hero.ToAttack(hit.collider);
+      }
+    }
+  }
+}
