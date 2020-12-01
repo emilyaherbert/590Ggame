197c49f31f23e423908ef6d422c3b6e9fe49abe8
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-01 01:14:05 -0500

    GameManager.cs

---
+++ game/Assets/Scripts/Classes/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+namespace HeroClash {
+  internal class GameManager : MonoBehaviour {
+    private const float DIST_EPSILON = 0.1f,
+                        SMOOTH_SPEED = 2.0f;
+    private readonly Vector3 OFFSET = (5 * Vector3.up) + (2.5f * Vector3.back);
+
+    // TODO: remove attr; set programmatically
+    [SerializeReference] internal Transform target = default;
+
+    private void LateUpdate() {
+      if (Vector3.Distance(transform.position,
+        target.position + OFFSET) > DIST_EPSILON) {
+        transform.position = Vector3.Lerp(transform.position,
+          target.position + OFFSET,
+          SMOOTH_SPEED * Time.deltaTime);
+      }
+    }
+  }
+}
