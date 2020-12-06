9478e75d7d4b886b2380b1209451e18028c150ae
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-06 15:59:09 -0500

    Wire-up character prefabs.

---
+++ game/Assets/Scripts/Classes/Prefab/HeroGolem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.AI;
+namespace HeroClash {
+  internal class HeroGolem : Hero {
+    private const float START_ACCEL = 8.0f,
+                        START_ATTACK = 1.7f,
+                        START_DAMAGE = 15.0f,
+                        START_HEALTH = 120.0f,
+                        START_MOVING = 5.0f;
+
+    protected override float AccelGain => 0.25f;
+    protected override float AttackLoss => 0.03f;
+    protected override float DamageGain => 3.0f;
+    protected override float HealthGain => 30.0f;
+    protected override float MovingGain => 0.2f;
+
+    private void Start() {
+      nav = GetComponent<NavMeshAgent>();
+      Self = new Stat(START_ACCEL,
+        START_ATTACK,
+        START_DAMAGE,
+        START_HEALTH,
+        START_MOVING);
+      nav.speed = Self.MoveSpeed;
+      nav.acceleration = Self.Accelerate;
+    }
+  }
+}
---
+++ game/Assets/Scripts/Classes/Prefab/HeroGrunt.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.AI;
+namespace HeroClash {
+  internal class HeroGrunt : Hero {
+    private const float START_ACCEL = 10.0f,
+                        START_ATTACK = 1.0f,
+                        START_DAMAGE = 10.0f,
+                        START_HEALTH = 100.0f,
+                        START_MOVING = 6.0f;
+
+    protected override float AccelGain => 0.5f;
+    protected override float AttackLoss => 0.02f;
+    protected override float DamageGain => 5.0f;
+    protected override float HealthGain => 20.0f;
+    protected override float MovingGain => 0.3f;
+
+    private void Start() {
+      nav = GetComponent<NavMeshAgent>();
+      Self = new Stat(START_ACCEL,
+        START_ATTACK,
+        START_DAMAGE,
+        START_HEALTH,
+        START_MOVING);
+      nav.speed = Self.MoveSpeed;
+      nav.acceleration = Self.Accelerate;
+    }
+  }
+}
