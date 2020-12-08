204e00debe0c5a0890dc818cbbf5a06f7be70412
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-12-07 23:14:55 -0500

    Removed unused ref to Player tag.

--- game/Assets/Scripts/Classes/Player.cs
+++ game/Assets/Scripts/Classes/Player.cs
@@ -8,14 +8,12 @@ namespace HeroClash {
       if (Input.GetMouseButtonDown(0) &&
           Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
             out RaycastHit hit) &&
-          !hit.collider.CompareTag("Player") &&
           !hit.collider.CompareTag("Character") &&
           !hit.collider.CompareTag("Structure")) {
         hero.Move(hit.point);
       } else if (Input.GetMouseButtonDown(1) &&
           Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),
             out hit) &&
-          !hit.collider.CompareTag("Player") &&
           (hit.collider.CompareTag("Character") ||
           hit.collider.CompareTag("Structure"))) {
         hero.ToAttack(hit.collider);
