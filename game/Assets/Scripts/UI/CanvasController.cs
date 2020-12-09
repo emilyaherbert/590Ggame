738e5a0d0837d1e9442ece3cd24254de5c05efb7
Emily Herbert <emilyherbert@cs.umass.edu>
2020-12-08 23:39:09 -0500

    health bar for golem works

--- game/Assets/Scripts/UI/CanvasController.cs
+++ game/Assets/Scripts/UI/CanvasController.cs
@@ -7,12 +7,6 @@ public class CanvasController : MonoBehaviour
 
     Vector3 trueRotation = new Vector3(90.0f, 0.0f, 0.0f);
 
-    void Update()
-    {
-        //transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
-        //transform.localRotation = Quaternion.Euler(-transform.parent.rotation.eulerAngles);
-    }
-    
     void LateUpdate() {
         transform.localRotation = Quaternion.Euler(-transform.parent.rotation.eulerAngles + trueRotation);
     }
---
+++ game/Assets/Scripts/UI/HeroGolemHealthBarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HeroClash {
+    internal class HeroGolemHealthBarController : MonoBehaviour {
+
+        //public Color backgroundColor;
+        //public Color barColor;
+        //public Color deadColor;
+
+        private Stat stat;
+        private Slider slider;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            stat = transform.parent.parent.GetComponentInChildren<HeroGolem>().Self;
+            slider = gameObject.GetComponent<Slider>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            SetHealthBarUI();
+        }
+
+        private void SetHealthBarUI() {
+            slider.value = stat.Health/stat.MaxHealth * 100.0f;
+        }
+    }
+}
