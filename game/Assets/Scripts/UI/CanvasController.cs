5ce944dd3f100c2f976dd80b6acc4d93fa6b51e3
Emily Herbert <emilyherbert@cs.umass.edu>
2020-12-08 23:09:47 -0500

    first pass at health bar UI

---
+++ game/Assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+
+    Vector3 trueRotation = new Vector3(90.0f, 0.0f, 0.0f);
+
+    void Update()
+    {
+        //transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+        //transform.localRotation = Quaternion.Euler(-transform.parent.rotation.eulerAngles);
+    }
+    
+    void LateUpdate() {
+        transform.localRotation = Quaternion.Euler(-transform.parent.rotation.eulerAngles + trueRotation);
+    }
+}
